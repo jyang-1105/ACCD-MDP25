a06d7d8a492b5c7ee70c00c26fd013424b9de1c4
jyang-1105 <jyang50@inside.artcenter.edu>
2025-10-10 12:50:58 -0700

    Update sketch.js

--- week4/sketch.js
+++ week4/sketch.js
@@ -1,12 +1,46 @@
+let posX
+let posY
+
+let VelX 
+let VelY
+
+let radius = 20
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600)
+
+  colorMode=(HSB, width, 100, 100)
+  posX = width*0.5
+  posY = height*0.5
+
+
+  VelX = -2
+  VelY = 3
 }
 
-function draw() {
-  background(220);
-  circle(width*0.5, height*0.5, 100);
-}
+ function draw() {
+  posX = posX + VelX
+  posY += VelY
 
-// function draw（） {
-//   background(220);
-// }
+  if (posY + radius >=height || posY -radius <=0){
+    VelY = VelY * -1
+  }
+
+  if (posX +radius >=width || posX -radius <=0){
+    VelX = VelX * -1
+  }
+   background(0, 0, 85)
+
+   noStroke()
+
+   fill(posX,0,250);
+
+   circle(posX, posY,radius * 2);
+
+   stroke(50,200,250);
+   strokeWeight(4);
+
+    fill(width * 0.75,100,100);
+   rect(width*0.5 -50, height*0.5 -50, 100, 100)
+ }
+
